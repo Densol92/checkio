151963b12707cdd31446ffd35a2eeca5c88c7dbd
densolovev <denis.solovev@plesk.com>
2016-12-19 18:53:11 +0700

    implement pawns

--- home/pawn_brotherhood.py
+++ home/pawn_brotherhood.py
@@ -5,8 +5,19 @@
 # Выходные данные: Количество защищенных пешек в виде целого числа.
 
 
+def get_defended(cell):
+    letter = ord(cell[0])
+    previous_row = str(int(cell[1]) - 1)
+    previos_letter, next_letter = letter - 1, letter + 1
+    return {chr(previos_letter) + previous_row, chr(next_letter) + previous_row}
+
+
 def safe_pawns(pawns):
-    return 0
+    protected = 0
+    for pawn in pawns:
+        if len(get_defended(pawn) & set(pawns)) > 0:
+            protected += 1
+    return protected
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
