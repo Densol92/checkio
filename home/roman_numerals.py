f104034c7a1cba79df635207def7cbf149824ec0
densolovev <denis.solovev@plesk.com>
2016-12-19 18:21:48 +0700

    roman solved

---
+++ home/roman_numerals.py
@@ -0,0 +1,43 @@
+# I 1 (unus)
+# V 5 (quinque)
+# X 10 (decem)
+# L 50 (quinquaginta)
+# C 100 (centum)
+# D 500 (quingenti)
+# M 1,000 (mille)
+
+mapa = {
+    '1000': 'M',
+    '900': 'CM',
+    '500': 'D',
+    '400': 'CD',
+    '100': 'C',
+    '90': 'XC',
+    '50': 'L',
+    '40': 'XL',
+    '10': 'X',
+    '9': 'IX',
+    '5': 'V',
+    '4': 'IV',
+    '1': 'I',
+}
+
+def checkio(data):
+    result = ''
+    keys = sorted([int(k) for k in mapa.keys()], reverse=True)
+    while data > 0:
+        print(data, result)
+        for next_sum in keys:
+            if data - next_sum >= 0:
+                data -= next_sum
+                result += mapa[str(next_sum)]
+                break
+    return result
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
+    assert checkio(3999) == 'MMMCMXCIX'
